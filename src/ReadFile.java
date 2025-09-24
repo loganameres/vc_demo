3ab702c67fb28aaeea5f41ca92ba4014d9df3d9a
loganameres <ljameres@gmail.com>
2025-09-24 11:06:15 -0400

    File Reader Updated

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,12 +1,11 @@
-import java.io.File;                  // Import the File class
-import java.io.FileNotFoundException; // Import this class to handle errors
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class ReadFile {
     public static void main(String[] args) {
         File myObj = new File("/Users/loganameres/Desktop/Topic 1/article1.txt");
 
-        // try-with-resources: Scanner will be closed automatically
         try (Scanner myReader = new Scanner(myObj)) {
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
